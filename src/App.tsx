e6924f2ad4a8c6d0c73968dfac1c5b51fe59db0a
Kseniya Smirnova <fanni@bk.ru>
2022-12-22 16:17:30 +0300

    feat: ui Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Github } from './Github';
 
 function App() {
   return (
     <div className="App">
-      
+      <Github/>
     </div>
   );
 }
--- src/Github.tsx
+++ src/Github.tsx
@@ -1,1 +1,23 @@
 import React from 'react';
+
+
+
+export const Github=()=> {
+  return (
+    <div>
+      <div>
+        <input placeholder='search'/>
+        <button>find</button>
+      </div>
+      <ul>
+        {['Dima', 'Kseniya'].map(n=><li>{n}</li>)}
+      </ul>
+      <div>
+        <h2>UserName</h2>
+        <div>Details</div>
+      </div>
+      
+    </div>
+  );
+}
+
