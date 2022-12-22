c42581be744c569c63da73dbde81c4efdb419a86
Kseniya Smirnova <fanni@bk.ru>
2022-12-22 16:35:52 +0300

    feat: useState

--- src/Github.tsx
+++ src/Github.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react'
+import s from './Github.module.css'
 
+export const Github = () => {
+  const[selectedUser, setSelectedUser]=useState<string | null>(null)
 
-
-export const Github=()=> {
   return (
-    <div>
+    <div className={s.container}>
       <div>
-        <input placeholder='search'/>
+        <input placeholder="search" />
         <button>find</button>
       </div>
       <ul>
-        {['Dima', 'Kseniya'].map(n=><li>{n}</li>)}
+        {['Dima', 'Kseniya'].map((u) => (
+          <li className={ selectedUser === u? s.selected : ''} 
+          onClick={()=>{
+            setSelectedUser(u)
+            document.title= u
+          }}>{u}</li>
+        ))}
       </ul>
       <div>
         <h2>UserName</h2>
         <div>Details</div>
       </div>
-      
     </div>
-  );
+  )
 }
-
