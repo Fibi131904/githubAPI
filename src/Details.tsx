01249b749d7a549e0ad6e7781a39f16c24696979
Kseniya Smirnova <fanni@bk.ru>
2022-12-26 12:19:54 +0300

    feat: Timer

--- src/Details.tsx
+++ src/Details.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { Timer } from './Timer'
 import { SearcUserType } from './UsesrList'
 
 export type UserType = {
@@ -13,22 +14,33 @@ type DetailsType = {
 }
 
 export const Details = (props: DetailsType) => {
+  const startTimerSeconds = 60
   const [userDetails, setUserDetails] = useState<null | UserType>(null)
+  const [seconds, setSeconds] = useState(startTimerSeconds)
 
   useEffect(() => {
     if (!!props.selectedUser) {
       axios
-        .get<UserType>(`https://api.github.com/users/${props.selectedUser.login}`)
+        .get<UserType>(
+          `https://api.github.com/users/${props.selectedUser.login}`
+        )
         .then((res) => {
+          setSeconds(startTimerSeconds)
           setUserDetails(res.data)
         })
     }
   }, [props.selectedUser])
+  useEffect(() => {
+    if (seconds < 1) {
+      setUserDetails(null)
+    }
+  }, [])
 
   return (
-    <div>      
-      {userDetails && (        
+    <div>
+      {userDetails && (
         <div>
+          <Timer seconds={seconds} onChange={setSeconds} timerKey={userDetails.id.toString()} />
           <h2>{userDetails.login}</h2>
           <img src={userDetails.avatar_url} />
           <br />
---
+++ src/Timer.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+
+type TimerPropsType = {
+  seconds: number
+  onChange: (actualSeconds: number) => void
+  timerKey: string
+}
+
+export const Timer = (props: TimerPropsType) => {
+  const [seconds, setSeconds] = useState(props.seconds)
+
+  useEffect(() => {
+    setSeconds(props.seconds)
+  }, [props.seconds])
+
+  useEffect(() => {
+    props.onChange(seconds)
+  }, [seconds])
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prev) => prev - 1)
+    }, 1000)
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [props.timerKey])
+
+  return <div>{seconds}</div>
+}
